1d9ab0813ad2133c4ea092895211a4978caca511
emilytao19 <emilytao@uw.edu>
2019-05-23 16:57:55 -0700

    adding alarmlist

---
+++ app/src/main/java/com/example/locationalarm/AlarmListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.locationalarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlarmListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_list)
+    }
+}
