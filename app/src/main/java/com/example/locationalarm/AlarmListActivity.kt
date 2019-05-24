e1033a56586848d9113862d910c94e8f4543aa0d
emilytao19 <emilytao@uw.edu>
2019-05-24 15:43:47 -0700

    more

--- app/src/main/java/com/example/locationalarm/AlarmListActivity.kt
+++ app/src/main/java/com/example/locationalarm/AlarmListActivity.kt
@@ -2,11 +2,35 @@ package com.example.locationalarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class AlarmListActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm_list)
+        val db = AlarmDatabase.getInstance(this)
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = AlarmAdapter(db.alarmDao().getAll())
+
+        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
+        }
     }
+    // ...
 }
