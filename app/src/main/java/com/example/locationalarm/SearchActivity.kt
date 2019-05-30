758075fc023418489fb2c8539af93f67b0a17636
Madeleine Stevenson <maps101@uw.edu>
2019-05-30 15:23:06 -0700

    Adds User Permissions and Map Initalizing to Search

--- app/src/main/java/com/example/locationalarm/SearchActivity.kt
+++ app/src/main/java/com/example/locationalarm/SearchActivity.kt
@@ -9,15 +9,26 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.Marker
 
-class SearchActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener  {
+    override fun onMarkerClick(p0: Marker?) = false
+
 
     private lateinit var mMap: GoogleMap
     private lateinit var latlng: LatLng
-    private lateinit var name: String
     private lateinit var location: String
-    private var radius: Double = 0.0
-    private lateinit var alert: String
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+
+    private val LOCATION_PERMISSION_REQUEST_CODE: Int = 1234
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,16 +37,27 @@ class SearchActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
 
-        //USE THIS TO PUT DATA FOR ALARM INTENT: intent.putExtra("package.name.key", "value");
-//        var extras = intent.extras //All Activities are started with an Intent
-//        val lat = extras.getDouble("latitude")
-//        val long = extras.getDouble("longitude")
-//        latlng = LatLng(lat, long)
-//        name = extras.getString("name")
-//        location = extras.getString( "location")
-//        radius = extras.getDouble("radius")
-//        alert = extras.getString("alert")
+    private fun getLocationPermission() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        mMap.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                lastLocation = location
+                latlng = LatLng(location.latitude, location.longitude)
+                mapSetMarker(mMap, latlng)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 12f))
+            }
+        }
     }
 
     /**
@@ -48,16 +70,20 @@ class SearchActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        mapSetMarker(mMap, LatLng(0.0, 0.0))
+
+        mMap.setOnMarkerClickListener(this)
+        mMap.uiSettings.isZoomControlsEnabled = true
+
+        getLocationPermission()
+
+        mapSetMarker(mMap, latlng)
     }
 
     //Set Marker for passed Alarm Intent
-    fun mapSetMarker(googleMap: GoogleMap, alarmLatlng: LatLng) {
-        mMap = googleMap
-
+    fun mapSetMarker(mMap: GoogleMap, alarmLatlng: LatLng) {
         // Add a marker at Alarm Location and move the camera
         val alarm = alarmLatlng
-        mMap.addMarker(MarkerOptions().position(alarm).title("Location Alarm Set Here"))
+        mMap.addMarker(MarkerOptions().position(alarm).title("Set Location Alarm Here"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(alarm))
     }
 }
