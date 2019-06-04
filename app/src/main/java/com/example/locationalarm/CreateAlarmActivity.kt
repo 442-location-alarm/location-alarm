acd1472355c01d45ee7cf869d85b186fd19de348
jlxp <joylxphillips@gmail.com>
2019-06-03 20:29:50 -0700

    CreateAlarmActivity files created and UI has been set up, functionality to come

---
+++ app/src/main/java/com/example/locationalarm/CreateAlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.example.locationalarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAlarmActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_alarm)
+    }
+}
