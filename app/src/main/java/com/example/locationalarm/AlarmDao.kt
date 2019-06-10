da311949e2b4b9b983ae17f4c7e722144b75b2e7
jamesbmc <jbmc@uw.edu>
2019-06-09 18:42:11 -0700

    more update functionality

--- app/src/main/java/com/example/locationalarm/AlarmDao.kt
+++ app/src/main/java/com/example/locationalarm/AlarmDao.kt
@@ -11,9 +11,18 @@ interface AlarmDao {
     @Insert
     fun insert(alarm: Alarm)
 
-    @Query("DELETE FROM alarms WHERE uid = :alarmId")
-    fun delete(alarmId: String)
+    @Query("DELETE FROM alarms WHERE uid = :alarmID")
+    fun delete(alarmID: String)
 
-    @Update
-    fun update(alarm: Alarm)
+    @Query("UPDATE alarms SET name = :name WHERE uid = :alarmID")
+    fun updateName(alarmID: String, name: String)
+
+    @Query("UPDATE alarms SET radius = :radius WHERE uid = :alarmID")
+    fun updateRadius(alarmID: String, radius: Double)
+
+    @Query("UPDATE alarms SET alert = :alert WHERE uid = :alarmID")
+    fun updateAlert(alarmID: String, alert: String)
+
+    @Query("UPDATE alarms SET location = :location, latitude = :latitude, longitude = :longitude WHERE uid = :alarmID")
+    fun updateLocation(alarmID: String, location: String, latitude: Double, longitude: Double)
 }
